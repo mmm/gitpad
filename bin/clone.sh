a5aef39011a2c57a4f9034fbc4a5365fed2aafc6
Mark Mims <mark.mims@canonical.com>
2011-12-14 16:10:26 -0700

    get one-way mirror working first

---
+++ bin/clone.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+repos=`~/bin/list-charms.rb`
+
+for repo in $repos
+do
+  git bzr clone $repo
+  charm_name=${repo//*\/}
+  echo $charm_name
+
+  if [ -d $charm_name ]; then
+    cd $charm_name
+    git create charms/$charm_name "Mirror of juju charm, pull requests and modifications welcome!" "http://charms.kapilt.com/charms/oneiric/$charm_name"
+    git remote add github git@github.com:charms/$charm_name
+    git push github master
+    cd ..
+  fi
+done
